4096d1109496ee749be0c8b731fdbe03bf626ce3
Pandas-1 <khemkavarun11@gmail.com>
2026-03-22 01:23:15 +0530

    fix: when moving quickly dots appear instead of lines

--- the_thing_that_does_the_thing.js
+++ the_thing_that_does_the_thing.js
@@ -14,6 +14,8 @@ let data = imageData.data;
 console.log(width)
 let mouse_x = 0
 let mouse_y = 0
+let interpolation_x = null;
+let interpolation_y = null;
 
 
 let isMouseDown = false;
@@ -26,7 +28,9 @@ document.addEventListener('mouseup', () => {
   isMouseDown = false;
 });
 
-let brush_color = [10 , 10, 255 , 100]
+//let brush_color = [10 , 10, 255 , 100]
+let brush_radius = 5;
+let brush_color = "#55bfda";
 
 //document.addEventListener('mousemove', function(event) {
     //console.log('Mouse X:', event.clientX, 'Mouse Y:', event.clientY);
@@ -34,6 +38,8 @@ let brush_color = [10 , 10, 255 , 100]
 //    mouse_y = event.clientY 
 //    console.log(mouse_x)    
 //});
+
+
 document.addEventListener('mousemove', function(event){
     if (isMouseDown && event.target === canvas){
     const xfactor = canvas.width / rect.width; //this is to scale the screen positioning to the canvas positioning
@@ -42,15 +48,30 @@ document.addEventListener('mousemove', function(event){
     mouse_y = event.clientY
     const canvas_x = Math.floor((mouse_x - rect.left)*xfactor)
     const canvas_y = Math.floor((mouse_y - rect.top)*yfactor)
-    //console.log(mouse_x, mouse_y)
+    
+    // console.log(mouse_x, mouse_y)
 
-    pixel_location = (canvas_y*canvas.width + canvas_x)*4
-    data[pixel_location]=brush_color[0]
-    data[pixel_location+1]=brush_color[1]
-    data[pixel_location+2]=brush_color[2]
-    data[pixel_location+3]=brush_color[3]
+    //pixel_location_array = (canvas_y*canvas.width + canvas_x)*4
+    //data[pixel_location_array]=brush_color[0]
+    //data[pixel_location_array+1]=brush_color[1]
+    //data[pixel_location_array+2]=brush_color[2]
+    //data[pixel_location_array+3]=brush_color[3]
     //console.log(ImageData.data[pixel_location],ImageData.data[pixel_location+1],ImageData.data[pixel_location+2],ImageData.data[pixel_location+3])
-    ctx.putImageData(imageData, 0, 0);
+    ctx.fillStyle = brush_color; 
+    ctx.strokeStyle = brush_color;
+    ctx.lineWidth = brush_radius*2
+    ctx.beginPath();
+    ctx.arc(canvas_x, canvas_y, brush_radius, 0, 2 * Math.PI);
+    ctx.fill();
+    if (interpolation_x !== null && interpolation_y !== null) {
+      ctx.beginPath();
+      ctx.moveTo(interpolation_x, interpolation_y);
+      ctx.lineTo(canvas_x, canvas_y);
+      ctx.stroke();
+    } 
+    interpolation_x = canvas_x
+    interpolation_y = canvas_y
+
   }
 });
 
